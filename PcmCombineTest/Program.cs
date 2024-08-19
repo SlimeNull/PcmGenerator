0b3dcb35916bb4440b1de889af777cef849a2988
SlimeNull <slimenull@outlook.com>
2024-08-19 09:33:22 +0800

    Add project files.

---
+++ PcmCombineTest/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var filePath1 = @"E:\CloudMusic\KSHMR,Mark Sixma - Gladiator (Remix).pcm";
+var filePath2 = @"E:\CloudMusic\西木康智 - バトル1.pcm";
+
+using var file1 = File.OpenRead(filePath1);
+using var file2 = File.OpenRead(filePath2);
+using var output = File.Create("output.pcm");
+
+using var reader1 = new BinaryReader(file1);
+using var reader2 = new BinaryReader(file2);
+using var outputWriter = new BinaryWriter(output);
+
+try
+{
+    while (true)
+    {
+        var value1 = reader1.ReadSingle();
+        var value2 = reader2.ReadSingle();
+        var outputValue = value1 / 2 + value2 / 2;
+
+        outputWriter.Write(outputValue);
+    }
+}
+catch { }
---
+++ PcmGenerator/Program.cs
@@ -0,0 +1,56 @@
+﻿using PcmGenerator;
+
+var pcm1Builder = PcmBuilder.CreateNew()
+    .WithSampleRate(44100)
+    .WithFrequency(8000);
+
+var pcm2Builder = PcmBuilder.CreateNew()
+    .WithSampleRate(44100)
+    .WithFrequency(3456);
+
+IEnumerable<float> CombinePcm(IEnumerable<float> numbers1, IEnumerable<float> numbers2)
+{
+    var enumerator1 = numbers1.GetEnumerator();
+    var enumerator2 = numbers2.GetEnumerator();
+
+    while (enumerator1.MoveNext() && enumerator2.MoveNext())
+    {
+        yield return enumerator1.Current / 2 + enumerator2.Current / 2;
+    }
+}
+
+using var output1 = File.Create("output1.wav");
+using var output2 = File.Create("output2.wav");
+using var output3 = File.Create("output3.wav");
+
+using var writer1 = new BinaryWriter(output1);
+using var writer2 = new BinaryWriter(output2);
+using var writer3 = new BinaryWriter(output3);
+
+var header = WaveHeader.Create(WaveAudioFormat.PCM, 1, 44100, 32, 44100 * 32);
+unsafe
+{
+    var headerByteSpan = new Span<byte>((byte*)&header, sizeof(WaveHeader));
+    writer1.Write(headerByteSpan);
+    writer2.Write(headerByteSpan);
+    writer3.Write(headerByteSpan);
+}
+
+var pcm1 = pcm1Builder.Build();
+var pcm2 = pcm2Builder.Build();
+var pcm3 = CombinePcm(pcm1Builder.Build(), pcm2Builder.Build());
+
+var pcm1Enumerator = pcm1.GetEnumerator();
+var pcm2Enumerator = pcm2.GetEnumerator();
+var pcm3Enumerator = pcm3.GetEnumerator();
+
+for (int i = 0; i < 44100 * 30; i++)
+{
+    pcm1Enumerator.MoveNext();
+    pcm2Enumerator.MoveNext();
+    pcm3Enumerator.MoveNext();
+
+    writer1.Write(pcm1Enumerator.Current);
+    writer2.Write(pcm2Enumerator.Current);
+    writer3.Write(pcm3Enumerator.Current);
+}
